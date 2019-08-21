e25a2e4136a715b715bd650ff21f9f4cdc61987f
fumiaki49 <me.fk39@gmail.com>
2019-08-21 20:55:31 +0900

    update users.rb

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user do
     email                  {"aaa@gmail.com"}
-    password               {"00000000"}
-    password_confirmation {"00000000"}
+    password               {"0000aaaa"}
+    password_confirmation  {"0000aaaa"}
   end
 end
