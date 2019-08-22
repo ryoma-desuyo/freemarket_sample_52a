45fd9b5cbb1e7849c2aafe17c6e649e7056f42bf
ryoma-desuyo <relicsjcbass4@gmail.com>
2019-08-22 21:28:25 +0900

    Update Basic認証

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,8 +10,8 @@ class ApplicationController < ActionController::Base
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
-      # username == "aaaa" && password == "2222"
+      # username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+      username == "52_mercari_wa" && password == "52wa"
     end
   end
 end
