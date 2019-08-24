12338a75f423ebe60a494b15782cc730310e6218
kometanimizuki <rpslym1026@ezweb.ne.jp>
2019-08-24 18:46:47 +0900

    Update products.rb

--- products.rb
+++ products.rb
@@ -1,3 +1,1 @@
-rename::    cannot open `rename:' (No such file or directory)
-product.rb: cannot open `product.rb' (No such file or directory)
 /dev/stdin:          ASCII text, with escape sequences
