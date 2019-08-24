b54af52e365f85a669a9375c2b1ff1fc74e52534
ryoma-desuyo <relicsjcbass4@gmail.com>
2019-08-24 12:04:26 +0900

    edit deploy function

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,6 @@ class ApplicationController < ActionController::Base
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
-      # username == "52_mercari_wa" && password == "52wa"
     end
   end
 end
