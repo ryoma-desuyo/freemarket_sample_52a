a96d84ef231c03e2e0911cb9f193ba6e35ee333e
kometanimizuki <rpslym1026@ezweb.ne.jp>
2019-08-24 17:35:48 +0900

    create products

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'products/index'
+  get 'products/create'
+  get 'products/new'
+  get 'products/edit'
+  get 'products/show'
+  get 'products/update'
+  get 'products/destroy'
   get 'product/index'
   get 'product/create'
   get 'product/new'
