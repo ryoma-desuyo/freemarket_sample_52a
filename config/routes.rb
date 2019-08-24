226f743fb0fd8e6fae65ac38c562d34e6a11df85
kometanimizuki <rpslym1026@ezweb.ne.jp>
2019-08-24 17:16:16 +0900

    cange controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'product/index'
+  get 'product/create'
+  get 'product/new'
+  get 'product/edit'
+  get 'product/show'
+  get 'product/update'
+  get 'product/destroy'
   devise_for :users
   root 'mercari#index'
 end
