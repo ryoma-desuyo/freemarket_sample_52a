c794add4a3052f4ce314497637f5be317301e01c
ryoma-desuyo <relicsjcbass4@gmail.com>
2019-08-24 17:09:08 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'mypages/index'
-  get 'mypages/edit'
-  get 'mypages/update'
   devise_for :users
   root 'mercari#index'
+  resources :mypages, only: [:index, :edit, :update]
 end
