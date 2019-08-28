bc016adc826f7baa9a4ae8c00babb801ab6d45b9
kometanimizuki <rpslym1026@ezweb.ne.jp>
2019-08-29 00:32:15 +0900

    users.scss

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -29,9 +29,6 @@ class Users::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
-  def index
-  end
-
   # GET /resource/cancel
   # Forces the session data which is usually expired after sign
   # in to be expired now. This is useful if the user wants to
