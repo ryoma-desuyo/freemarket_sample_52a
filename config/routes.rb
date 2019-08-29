e63542e8caeb15d262dca0befad4807cf9c4b9f3
kometanimizuki <rpslym1026@ezweb.ne.jp>
2019-08-30 00:47:20 +0900

    Change routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  resources :products
+  get 'users/new'
+
   devise_for :users
   resources :mypages, only: [:index, :edit, :update]
+  resources :products
   root 'products#index'
 end
