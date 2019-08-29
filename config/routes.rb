7d6f68664237ad869e01ae0f25913bffaa430ec8
kometanimizuki <rpslym1026@ezweb.ne.jp>
2019-08-30 00:26:23 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,5 @@
 Rails.application.routes.draw do
-  get 'users/new'
-  get 'products/index'
-  get 'products/create'
-  get 'products/new'
-  get 'products/edit'
-  get 'products/show'
-  get 'products/update'
-  get 'products/destroy'
-
+  resources :products
   devise_for :users
   resources :mypages, only: [:index, :edit, :update]
   root 'products#index'
