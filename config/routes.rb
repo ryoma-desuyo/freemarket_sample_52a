6da5a00df311709d5cc814b2934f2a8ca70ef290
kometanimizuki <rpslym1026@ezweb.ne.jp>
2019-08-31 22:12:12 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,13 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
-  devise_for :users
-  resources :mypages, only: [:index, :edit, :update]
-  resources :products
-  root 'products#index'
   devise_for :users, controllers: {
     registrations: "users/registrations"
   }
-
   as :user do
     get "sign_up/number", to:"users/registrations#number"
     get "sign_up/address", to: "users/registrations#address"
     get "sign_up/credit", to: "users/registrations#credit"
     get "/sign_up/done", to:"users/registrations#done"
   end
-
+  resources :products
+  resources :mypages, only: [:index, :edit, :update]
 end
