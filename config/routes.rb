91cb1d12cca69001b00bbda306461172794ef2b3
kometanimizuki <rpslym1026@ezweb.ne.jp>
2019-08-31 22:06:29 +0900

    confrict

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,15 @@ Rails.application.routes.draw do
   resources :mypages, only: [:index, :edit, :update]
   resources :products
   root 'products#index'
+  devise_for :users, controllers: {
+    registrations: "users/registrations"
+  }
+
+  as :user do
+    get "sign_up/number", to:"users/registrations#number"
+    get "sign_up/address", to: "users/registrations#address"
+    get "sign_up/credit", to: "users/registrations#credit"
+    get "/sign_up/done", to:"users/registrations#done"
+  end
+
 end
