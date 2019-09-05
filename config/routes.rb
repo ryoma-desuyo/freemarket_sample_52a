590c6c63b588b92a9a8479bf36e32427fa4372b4
fumiaki49 <me.fk39@gmail.com>
2019-09-05 21:04:03 +0900

    各ページでParametersを作成してcreate action でデータベースに保存

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,11 @@ Rails.application.routes.draw do
     registrations: "users/registrations"
   }
   as :user do
-    get "sign_up/number", to:"users/registrations#number"
-    get "sign_up/address", to: "users/registrations#address"
+    get "/sign_up/basic_info", to: "users/registrations#basic_info"
+    post "sign_up/number", to:"users/registrations#number"
+    post "sign_up/address", to: "users/registrations#address"
     get "sign_up/credit", to: "users/registrations#credit"
+    post "/sign_up/completed" to:"users/registrations#create"
     get "/sign_up/done", to:"users/registrations#done"
   end
   
