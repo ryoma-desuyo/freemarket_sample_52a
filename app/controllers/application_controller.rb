b52dfa52a1fcce1cdfc1a18221cd65f10a043059
fumiaki49 <me.fk39@gmail.com>
2019-09-07 12:31:00 +0900

    ログイン後、ログアウト後のリダイレクト先を指定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,14 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :congigure_permitted_parameters, if: :devise_controller?
 
+  def after_sign_in_path_for(resource)
+    mypages_path
+  end
+
+  def after_sign_out_path_for(resource)
+    products_path
+  end
+
   private
 
   def production?
