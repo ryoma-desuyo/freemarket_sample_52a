4331e0c7debae1c6d674cd8c6524fe56e287de7c
ryoma-desuyo <relicsjcbass4@gmail.com>
2019-09-16 20:11:36 +0900

    Update products_controller.rb

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -31,6 +31,9 @@ class ProductsController < ApplicationController
   end
 
   def destroy
+    @product = Product.find_by(id: params[:id])
+    @product.destroy
+    redirect_to("/")
   end
 
 private
