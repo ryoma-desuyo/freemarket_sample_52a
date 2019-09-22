f0d4dfc4338a0d7e1bf12ca2422a89e8c818a8e7
ryoma-desuyo <relicsjcbass4@gmail.com>
2019-09-22 15:53:09 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,10 +11,7 @@ class User < ApplicationRecord
   has_many :cards
 
   has_many :buyed_products, foreign_key: "buyer_id", class_name: "Product"
-  #userが買った商品
   has_many :selling_products, -> { where("buyer_id is NULL") }, foreign_key: "seller_id", class_name: "Product"
-  #userが現在売っている商品
   has_many :sold_products, -> { where("buyer_id is not NULL") }, foreign_key: "seller_id", class_name: "Product"
-  #売り切れた商品
 
 end
