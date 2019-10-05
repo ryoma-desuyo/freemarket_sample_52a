c2e030e5d9d33cbe16ef5c07597f5236ff7ab4fa
kometanimizuki <rpslym1026@ezweb.ne.jp>
2019-10-05 12:25:28 +0900

    Change model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :omniauthable
 
   validates :password, presence: true, length: {in: 7..128}, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
 
@@ -14,4 +14,19 @@ class User < ApplicationRecord
   has_many :selling_products, -> { where("buyer_id is NULL") }, foreign_key: "seller_id", class_name: "Product"
   has_many :sold_products, -> { where("buyer_id is not NULL") }, foreign_key: "seller_id", class_name: "Product"
 
-end
+  devise   :omniauthable,omniauth_providers: [:facebook, :google_oauth2]
+  has_many :sns_credentials, dependent: :destroy
+
+  def self.find_for_google_oauth2(auth)
+    user = User.where(email: auth.info.email).first
+ 
+    unless user
+      user = User.create(name:     auth.info.name,
+                         provider: auth.provider,
+                         uid:      auth.uid,
+                         email:    auth.info.email,
+                         token:    auth.credentials.token,
+                         password: Devise.friendly_token[0, 20])
+    end
+    user
+  end
