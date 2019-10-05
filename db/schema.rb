f0109be5756baee0d37035f8347e32b286dd1133
kometanimizuki <rpslym1026@ezweb.ne.jp>
2019-10-05 12:26:08 +0900

    Change database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_25_070353) do
+ActiveRecord::Schema.define(version: 2019_10_01_004632) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "prefecture_id"
@@ -53,12 +53,11 @@ ActiveRecord::Schema.define(version: 2019_09_25_070353) do
   end
 
   create_table "sns_credentials", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "provider"
+    t.integer "user_id"
     t.string "uid"
-    t.bigint "user_id"
+    t.string "provider"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_sns_credentials_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -90,5 +89,4 @@ ActiveRecord::Schema.define(version: 2019_09_25_070353) do
   end
 
   add_foreign_key "cards", "users"
-  add_foreign_key "sns_credentials", "users"
 end
