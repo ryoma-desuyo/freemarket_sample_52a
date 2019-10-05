10ddbef14235b602c9f09b3326d709f1e0c83093
kometanimizuki <rpslym1026@ezweb.ne.jp>
2019-10-05 18:05:17 +0900

    Change models

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :omniauthable
+         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: %i[facebook google_oauth2]
 
   validates :password, presence: true, length: {in: 7..128}, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
 
@@ -14,19 +14,10 @@ class User < ApplicationRecord
   has_many :selling_products, -> { where("buyer_id is NULL") }, foreign_key: "seller_id", class_name: "Product"
   has_many :sold_products, -> { where("buyer_id is not NULL") }, foreign_key: "seller_id", class_name: "Product"
 
-  devise   :omniauthable,omniauth_providers: [:facebook, :google_oauth2]
-  has_many :sns_credentials, dependent: :destroy
-
-  def self.find_for_google_oauth2(auth)
-    user = User.where(email: auth.info.email).first
- 
-    unless user
-      user = User.create(name:     auth.info.name,
-                         provider: auth.provider,
-                         uid:      auth.uid,
-                         email:    auth.info.email,
-                         token:    auth.credentials.token,
-                         password: Devise.friendly_token[0, 20])
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create(birthday: 00000000) do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
     end
-    user
   end
+end
