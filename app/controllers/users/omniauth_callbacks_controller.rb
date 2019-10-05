7d85f38e1e9c31c7f6b45e00f8f6144790179941
kometanimizuki <rpslym1026@ezweb.ne.jp>
2019-10-05 18:05:24 +0900

    Update omniauth_callbacks_controller.rb

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,33 +1,25 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-  def facebook
-    callback_for(:facebook)
-  end
-
   def google_oauth2
-    @user = User.find_for_google_oauth2(request.env["omniauth.auth"])
- 
-    if @user.persisted?
-      flash[:notice] = I18n.t "devise.omniauth_callbacks.success", kind: "Google"
-      sign_in_and_redirect @user, event: :authentication
-    else
-      session["devise.google_data"] = request.env["omniauth.auth"]
-      redirect_to new_user_registration_url
-    end
+    callback_for :google
   end
-end
+  
+  def facebook
+    callback_for :facebook
+  end
 
   def callback_for(provider)
-    @user = User.find_oauth(request.env["omniauth.auth"])
+    @user = User.from_omniauth(request.env["omniauth.auth"])
     if @user.persisted?
-      sign_in_and_redirect @user, event: :authentication #after_sign_in_path_forと同じパス
+      sign_in_and_redirect @user, event: :authentication #this will throw if @user is not activated
       set_flash_message(:notice, :success, kind: "#{provider}".capitalize) if is_navigational_format?
     else
       session["devise.#{provider}_data"] = request.env["omniauth.auth"].except("extra")
-      redirect_to signup_registration_path
+
     end
   end
 
   def failure
-    redirect_to root_path and return
+    redirect_to root_path
   end
+
 end
