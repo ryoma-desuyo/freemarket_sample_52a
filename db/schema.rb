1a3381143a0759fe38deb313a9b1149b83d3d448
kometanimizuki <rpslym1026@ezweb.ne.jp>
2019-10-05 18:04:09 +0900

    Add gem

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_01_004632) do
+ActiveRecord::Schema.define(version: 2019_09_30_073521) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "prefecture_id"
@@ -50,14 +50,6 @@ ActiveRecord::Schema.define(version: 2019_10_01_004632) do
     t.integer "buyer_id", default: 0
     t.datetime "created_at"
     t.datetime "updated_at"
-  end
-
-  create_table "sns_credentials", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.integer "user_id"
-    t.string "uid"
-    t.string "provider"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
