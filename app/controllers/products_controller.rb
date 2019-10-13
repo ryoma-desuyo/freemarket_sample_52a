b95b1e0c5e29e439777d11cca629aa935162357f
kominaminaoki <pinkypinky0331@gmail.com>
2019-10-13 17:57:37 +0900

    Update products_controller.rb

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -69,7 +69,9 @@ class ProductsController < ApplicationController
   end
 
   def buying
-
+    if current_user.selling_products.present? && current_user.selling_products.find_by(id: params[:id]).present?
+      redirect_to comfirm_product_path(id: params[:id]), alert: '自分が出品した商品の購入はできません'
+    end
   end
 
   def result
