4faf0b2d877dbbbdb54e85312ce0c57ea9dc5ac5
kometanimizuki <rpslym1026@ezweb.ne.jp>
2019-10-13 16:40:07 +0900

    Change omniauthable

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -14,7 +14,6 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       set_flash_message(:notice, :success, kind: "#{provider}".capitalize) if is_navigational_format?
     else
       session["devise.#{provider}_data"] = request.env["omniauth.auth"].except("extra")
-
     end
   end
 
