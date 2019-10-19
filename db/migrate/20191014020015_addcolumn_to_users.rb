bc14180fc0b1d4f7688d3d849ad7f8b36dc00723
kometanimizuki <rpslym1026@ezweb.ne.jp>
2019-10-19 12:44:38 +0900

    Get migrationFile

--- db/migrate/20191014020015_addcolumn_to_users.rb
+++
@@ -1,7 +0,0 @@
-class AddcolumnToUsers < ActiveRecord::Migration[5.2]
-  def change
-    change_column :users, :birthday, :date
-    remove_column :users, :provider
-    remove_column :users, :uid
-  end
-end
