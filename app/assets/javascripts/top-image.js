4e48b7cefd9f2bfab3b0ffc73a3e776966c4e6f7
kometanimizuki <rpslym1026@ezweb.ne.jp>
2019-10-20 12:13:01 +0900

    Add image

---
+++ app/assets/javascripts/top-image.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+  $("top-img").topSwither ({
+    images: ["top-image/maining.png","top-image/top1.png", "top-image/top2.png"],
+    auto: true,
+    pause: 5000,
+  });
+});
